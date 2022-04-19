85acefb7637d59b759ec45fce8f57c6e9264ab30
adrianve <adrianve@pm.me>
2022-04-19 10:42:20 -0500

    feat: component for Swipe Buttons
    add component to make custom buttons that appear with swipeable tasks

---
+++ components/SwipeButton.tsx
@@ -0,0 +1,31 @@
+import { BorderlessButton } from 'react-native-gesture-handler';
+import { View, Text } from './Themed'
+import styles from '../styles'
+import useTheme from '../hooks/useTheme';
+
+interface SwipeButtonProps {
+    onPress: (pointerInside: boolean) => void
+    color?: string
+    backgroundColor?: string
+    text?: string
+    size?: number
+}
+
+export default (props: SwipeButtonProps) => {
+    const { onPress, color, backgroundColor, text, size } = props
+    const theme = useTheme()
+    const appliedColor = color ? color : theme.color
+    const appliedBgColor = backgroundColor ? backgroundColor : theme.backgroundColor
+    const appliedText = text ? text : 'Press'
+    const appliedSize = size ? size : 30
+    
+    return(
+        <BorderlessButton onPress={onPress}>
+            <View style={[{flex: 1, alignItems: 'flex-end', justifyContent: 'center', backgroundColor: appliedBgColor, paddingLeft: appliedSize, paddingRight: appliedSize,}]}>
+                <Text style={{color: appliedColor}}>
+                    {appliedText}
+                </Text>
+            </View>
+        </BorderlessButton>
+    )
+}
--- components/Task.tsx
+++ components/Task.tsx
@@ -1,12 +1,14 @@
 import { TaskProps, CompletedTaskProps, ReadonlyTask as ReadonlyTaskType, CompletedTask as CompletedTaskType, Place } from '../types';
 import { Text, View } from './Themed'
-import styles from '../styles'
 import { getPlaceElement } from './Place'
-import { Swipeable } from 'react-native-gesture-handler';
+import { Swipeable } from 'react-native-gesture-handler'
+import { Alert, Pressable } from 'react-native'
+import SwipeButton from './SwipeButton'
+import styles from '../styles'
 
 const LeftSwipeAction = () => {
     return (
-        <View style={[styles.taskList, {alignItems: 'flex-end', justifyContent: 'center', backgroundColor: '#7f7'}]}>
+        <View style={[{alignItems: 'flex-end', justifyContent: 'center', backgroundColor:'#7f7', paddingLeft: 30, paddingRight: 30,}]}>
             <Text>
                 ✔
             </Text>
@@ -14,13 +16,12 @@ const LeftSwipeAction = () => {
     )
 }
 
-const RightSwipeAction = () => {
+const RightSwipeButtons = () => {
     return (
-        <View style={[styles.taskList, {flex: 1, alignItems: 'flex-end', justifyContent: 'center', backgroundColor: '#f55'}]}>
-            <Text>
-                🗑
-            </Text>
-        </View>
+        <>
+            <SwipeButton onPress={() => Alert.alert('Item deleted')} text={'🗑'} backgroundColor='#f77' ></SwipeButton>
+            <SwipeButton onPress={() => Alert.alert('Editing Item')} text={'✏'} ></SwipeButton>
+        </>
     )
 }
 
@@ -30,37 +31,45 @@ export const Task = (props: TaskProps & {toggle: Function}) => {
     const swipeFromLeftOpen = () => {
         toggle(task)
     }
-    const swipeFromRightOpen = () => {
-        alert('delete')
-    }
 
+    return (
+            <Swipeable
+                renderLeftActions={LeftSwipeAction}
+                onSwipeableLeftWillOpen={swipeFromLeftOpen}
+                renderRightActions={RightSwipeButtons}
+            >
+                <Pressable onPress={() => Alert.alert('Task Pressed')}>
+                    <View style={[styles.taskList, {flex: 1}]}>
+                        <Text style={[styles.fontMain]}>
+                            {task.text}
+                        </Text>
+                        {getPlaceElement(task.place as Place)}
+                    </View>
+                </Pressable>
+            </Swipeable>
+    );
+}
+
+export const CompletedTask = (props: CompletedTaskProps & {toggle: Function}) => {
+    const { style, task, toggle, ...otherProps } = props
+
+    const swipeFromLeftOpen = () => {
+        toggle(task)
+    }
+    
     return (
         <Swipeable
             renderLeftActions={LeftSwipeAction}
-            onSwipeableLeftOpen={swipeFromLeftOpen}
-            renderRightActions={RightSwipeAction}
-            onSwipeableRightOpen={swipeFromRightOpen}
+            onSwipeableLeftWillOpen={swipeFromLeftOpen}
+            renderRightActions={RightSwipeButtons}
         >
-            <View style={[styles.taskList]}>
-                <Text style={[styles.fontMain]}>
+            <View style={[styles.taskList, {flex: 1}]}>
+                <Text style={[styles.completedTask, styles.fontMain]} >
                     {task.text}
                 </Text>
                 {getPlaceElement(task.place as Place)}
             </View>
         </Swipeable>
-    );
-}
-
-export const CompletedTask = (props: CompletedTaskProps) => {
-    const { style, task, ...otherProps } = props
-    
-    return (
-      <View style={[styles.taskList]}>
-        <Text style={[styles.completedTask, styles.fontMain]} >
-            {task.text}
-        </Text>
-        {task.place && getPlaceElement(task.place as Place)}
-      </View>
     );
   }
 
@@ -68,6 +77,6 @@ export const getTaskElement = (task: ReadonlyTaskType | CompletedTaskType, handl
     if(!task.done) {
         return <Task task={task as ReadonlyTaskType} toggle={handleToggleTask} />
     } else {
-        return <CompletedTask task={task as CompletedTaskType} />
+        return <CompletedTask task={task as CompletedTaskType} toggle={handleToggleTask} />
     }
 }
