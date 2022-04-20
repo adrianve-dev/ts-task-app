423b0feb2c6230386ae465e00803778321a51dcc
adrianve <adrianve@pm.me>
2022-04-20 10:53:28 -0500

    feat: add AsyncStorage api
    - edit Ap.tsx to test AsyncStorage

--- App.tsx
+++ App.tsx
@@ -1,45 +1,51 @@
 import * as React from 'react';
-import { FlatList, ListRenderItem, SafeAreaView, StyleSheet } from 'react-native';
+import { FlatList, ListRenderItem, ListRenderItemInfo, Pressable, SafeAreaView, StyleSheet } from 'react-native';
 import { Text, View } from './components/Themed'
 import useTheme from './hooks/useTheme';
-import { CompletedTask, ReadonlyTask } from './types';
+import { CompletedTask, ReadonlyTask } from './types'
 import { getTaskElement } from './components/Task'
-import { toggleTask } from './utils/utils';
-
+import { toggleTask } from './utils/utils'
+import { BorderlessButton } from 'react-native-gesture-handler'
+import { asyncAddTask, asyncDeleteData, asyncUpdateTaskCount, getStoredTasks, getTaskCount } from './utils/api'
+import { colors } from './styles';
 
 export default function App() {
-  const [tasks, setTasks] = React.useState<ReadonlyTask[]>([
-    {
-      id: 0,
-      text: 'Do the Laundry',
-      done: false,
-      place: 'home'
-    },
-    {
-      id: 1,
-      text: 'Do the analysis report',
-      done: false,
-      place: 'work'
-    },
-    {
-      id: 2,
-      text: 'Hit all PRs',
-      done: false,
-    },
-    {
-      id: 3,
-      text: 'Hit all PRs',
-      done: true,
-      place: { custom: 'gym' },
-    },
-  ])
+  // key: number but storedKeys stored as key: string
+  // handleToggleTask throws type error when change above to : string 
+  const [tasks, setTasks] = React.useState<{[key:number]: ReadonlyTask}>({
+  })
+  const [taskAdded, setTaskAdded] = React.useState<boolean>(false)
 
-  const theme = useTheme()
-  const DATA: ReadonlyTask[] = tasks
+  // asyncDeleteData()
+
+  const addTask = async () => {
+    const count = await getTaskCount()
+    // assume null/undefined mean no tasks
+    const id: number = count !== null && typeof count !== 'undefined' ? count : 0
+    await asyncAddTask({
+      id: id,
+      text: `This is new task #${id}`,
+      done: false,
+    })
+
+    await asyncUpdateTaskCount()
+    setTaskAdded(true)
+  }
+
+  const formatData = (data: {[key: string]: ReadonlyTask}): ReadonlyTask[] => {
+    let allTasks: ReadonlyTask[] = []
+    Object.keys(tasks).forEach((t: string) => {
+      const nextTask = data[t]
+      allTasks.push(nextTask)
+    })
+    return allTasks
+  }
+  
+  const DATA: ReadonlyTask[] = formatData(tasks)
 
   const handleToggleTask = (task: ReadonlyTask) => {
     const updatedTask: ReadonlyTask = toggleTask(task)
-    const updatedTasks: ReadonlyTask[] = tasks.map((t) => {
+    const updatedTasks: ReadonlyTask[] = formatData(tasks).map((t) => {
       if(t.id === task.id) return updatedTask
       else return t
     })
@@ -47,17 +53,63 @@ export default function App() {
     setTasks(updatedTasks)
   }
 
-  const renderItem: ListRenderItem<ReadonlyTask | CompletedTask> = ({ item }) => getTaskElement(item, handleToggleTask)
+  const renderItem: ListRenderItem<ReadonlyTask | CompletedTask> = ({ item, index }) => getTaskElement(item, handleToggleTask)
   
+  // update state on add task
+  React.useEffect(() => {
+    const updateData = async () => {
+      const storedTasks = await getStoredTasks()
+      console.log('stored tasks: ', storedTasks)
+
+      const allTasks = Object.assign({}, tasks, storedTasks)
+      if(Object.keys(allTasks).length > Object.keys(tasks).length) setTasks(allTasks)
+      setTaskAdded(false)
+    }
+
+    if(taskAdded) {
+      updateData()
+    }
+  }, [taskAdded])
+
+  // initialize data on load
+  React.useEffect(() => {
+    const initData = async () => {
+      console.log('init data')
+      const count = await getTaskCount()
+      const storedTasks = await getStoredTasks()
+      
+      if(count === null) {
+        if(storedTasks === null){
+          await asyncUpdateTaskCount('0')
+        } else {
+          await asyncUpdateTaskCount(Object.keys(storedTasks).length.toString())
+        }
+      }
+
+      const allTasks = Object.assign({}, tasks, storedTasks)
+      if(Object.keys(allTasks).length > Object.keys(tasks).length) setTasks(allTasks)
+    }
+    initData()
+  }, [])
+
   return (
-    <SafeAreaView style={[styles.container, {backgroundColor:'#282c34'}]}>
+    <SafeAreaView style={[styles.container, {backgroundColor: colors.reactDarkBackground}]}>
       <View style={styles.list}>
-        <FlatList<ReadonlyTask | CompletedTask>
+        {Object.keys(tasks).length > 0 ? <FlatList<ReadonlyTask | CompletedTask>
             data={DATA}
-            renderItem={renderItem}
+            renderItem={(item: ListRenderItemInfo<ReadonlyTask | CompletedTask>) => renderItem(item)}
+            keyExtractor={(item, index) => index.toString()}
           >
-        </FlatList>
+        </FlatList> 
+        : <View style={[{flex: 1, alignItems: 'center', paddingTop: 60}]}><Text style={{fontSize: 18}}>No Tasks</Text></View>}
       </View>
+      <BorderlessButton style={{flex:1, marginLeft: 10, marginRight: 10,}} onPress={() => addTask()} >
+        <View style={[{flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'blue', borderRadius: 10,}]}>
+                <Text style={{fontSize: 18}}>
+                    Add Task
+                </Text>
+            </View>
+          </BorderlessButton>
     </SafeAreaView>
   );
 }
@@ -74,6 +126,6 @@ const styles = StyleSheet.create({
     marginBottom: 15,
   },
   list: {
-    flex: 1,
+    flex: 9,
   }
 });
---
+++ utils/api.ts
@@ -0,0 +1,82 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Alert } from 'react-native';
+import { ReadonlyTask, CompletedTask } from '../types';
+
+export const TASKS_STORAGE_KEY = 'adrianve::tasks'
+export const TASK_COUNTER = 'adrianve::counter'
+
+export const getStoredTasks = async () => {
+    try {
+        const jsonValue = await AsyncStorage.getItem(TASKS_STORAGE_KEY)
+        return jsonValue !== null ? JSON.parse(jsonValue) : null
+    } catch (e) {
+        Alert.alert('Failed to get Tasks')
+    }
+}
+
+export const asyncAddTask = async (task: ReadonlyTask) => {
+    try {
+        return await saveTask(task)
+    } catch (e) {
+        Alert.alert('Failed to add Task')
+    }
+}
+
+export const asyncUpdateTask = async (tasks: ReadonlyTask[]) => {
+    try {
+        return await AsyncStorage.setItem(TASKS_STORAGE_KEY,
+            JSON.stringify(tasks))
+    } catch (e) {
+        Alert.alert('Failed to update Task') 
+    }
+}
+
+export const asyncCompleteTask = async (task: CompletedTask) => {
+    try {
+        return await saveTask(task)
+    } catch (e) {
+        Alert.alert('Failed to add Task')
+    }
+}
+
+const saveTask = async (task: ReadonlyTask) => {
+    return await AsyncStorage.mergeItem(TASKS_STORAGE_KEY,
+        JSON.stringify({
+            [task.id.toString()]: task
+        })
+    )
+}
+
+export const getTaskCount = async (): Promise<number | null | undefined> => {
+    try {
+        const jsonValue = await AsyncStorage.getItem(TASK_COUNTER)
+        return jsonValue !== null ? parseInt(jsonValue) : null
+    } catch (e) {
+        Alert.alert('Failed to get task count')
+    }
+}
+
+/** use to initialize
+ *  - if self null
+ *  - if storedTasks null
+ *  - then initialize to 0 w/ manual argument
+ */
+export const asyncUpdateTaskCount = async (taskCount?: string) => {
+    try {
+        // set manual
+        if(taskCount && Number(taskCount) >= 0) {
+            return await AsyncStorage.setItem(TASK_COUNTER, taskCount)
+        }
+
+        // increment
+        const jsonValue = await AsyncStorage.getItem(TASK_COUNTER)
+        // assumes null because of no entry
+        const count = jsonValue !== null ? parseInt(jsonValue) : 0 
+        const value = (count + 1).toString()
+        return await AsyncStorage.setItem(TASK_COUNTER, value)
+    } catch (e) {
+        Alert.alert('Failed to update task count') 
+    }
+}
+
+export const asyncDeleteData = async () => await AsyncStorage.clear()
