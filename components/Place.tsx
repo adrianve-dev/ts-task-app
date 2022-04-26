c6faca872326dd0f6c8f4ac278cc6f1dea2e67e1
adrianve <adrianve@pm.me>
2022-04-26 10:17:18 -0500

    refactor: add icon to PlaceNone
    - PlaceNone shows a plus icon instead of a text plus sign

--- components/Place.tsx
+++ components/Place.tsx
@@ -2,15 +2,15 @@ import { DefaultViewTextProps, Place as PlaceType, PlaceProps } from "../types"
 import { Text, View } from './Themed'
 import { styles } from '../styles'
 import { placeToDisplayString } from '../utils/utils';
-
-
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export const PlaceNone = (props: DefaultViewTextProps ) => {
     const { style, ...otherProps } = props
     return (
         <View style={{alignItems:'center'}}>
             <Text style={[styles.place, styles.fontSubtitle]} >
-                + Place
+                <Ionicons name={'ios-add-circle-sharp'} color='white'/>
+                 Place
             </Text>
         </View>
     )
