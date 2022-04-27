32c4c3cef59a74f24e2152f38da0f0276a871604
adrianve <adrianve@pm.me>
2022-04-27 10:21:25 -0500

    refactor: themed Place component

--- components/Place.tsx
+++ components/Place.tsx
@@ -1,15 +1,26 @@
+import { useColorScheme, ColorSchemeName } from "react-native"
 import { DefaultViewTextProps, Place as PlaceType, PlaceProps } from "../types"
 import { Text, View } from './Themed'
-import { styles } from '../styles'
-import { placeToDisplayString } from '../utils/utils';
-import Ionicons from '@expo/vector-icons/Ionicons';
+import { colors, styles } from '../styles'
+import { placeToDisplayString } from '../utils/utils'
+import Ionicons from '@expo/vector-icons/Ionicons'
+
+const getColors = (colorScheme: ColorSchemeName): [string, string] => {
+    const backgroundColor = colorScheme === 'light' ? colors.offWhite : colors.dark
+    const color = colorScheme === 'light' ? 'black' : 'white'
+    return [color, backgroundColor]
+}
 
 export const PlaceNone = (props: DefaultViewTextProps ) => {
     const { style, ...otherProps } = props
+    const colorScheme = useColorScheme() as NonNullable<ColorSchemeName>
+
+    const [color, backgroundColor] = getColors(colorScheme)
+
     return (
         <View style={{alignItems:'center'}}>
-            <Text style={[styles.place, styles.fontSubtitle]} >
-                <Ionicons name={'ios-add-circle-sharp'} color='white'/>
+            <Text style={[styles.place, styles.fontSubtitle, { color, backgroundColor, }]} >
+                <Ionicons name={'ios-add-circle-sharp'} color={color}/>
                  Place
             </Text>
         </View>
@@ -18,9 +29,13 @@ export const PlaceNone = (props: DefaultViewTextProps ) => {
 
 export const Place = (props: PlaceProps) => {
     const { style, place, ...otherProps } = props
+    const colorScheme = useColorScheme() as NonNullable<ColorSchemeName>
+
+    const [color, backgroundColor] = getColors(colorScheme)
+
     return (        
         <View style={{alignItems:'center'}}>
-            <Text style={[styles.place, styles.fontSubtitle]} >
+            <Text style={[styles.place, styles.fontSubtitle, { color, backgroundColor, }]} >
                 {place && placeToDisplayString(place)}
             </Text>
         </View>
